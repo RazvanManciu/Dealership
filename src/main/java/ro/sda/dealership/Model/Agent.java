7ed2fa741512089b1599fda31368f45553d41ce8
Sorin TKD <sorinmiron@gmail.com>
2019-02-10 09:22:27 +0200

    constructori si settere Agent

--- src/main/java/ro/sda/dealership/Model/Agent.java
+++ src/main/java/ro/sda/dealership/Model/Agent.java
@@ -3,4 +3,25 @@ package ro.sda.dealership.Model;
 public class Agent {
     Integer agentID;
     String agentName;
+
+    public Agent(Integer agentID, String agentName) {
+        this.agentID = agentID;
+        this.agentName = agentName;
+    }
+
+    public Integer getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(Integer agentID) {
+        this.agentID = agentID;
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
 }
